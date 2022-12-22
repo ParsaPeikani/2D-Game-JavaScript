14b29e64b8f1d2598f09323b5bd082e8a8e46f90
Parsa Peikani <a12365@Parsas-MacBook-Air.local>
2022-12-21 16:22:53 -0800

    Addign game time to our game

--- script.js
+++ script.js
@@ -132,13 +132,34 @@ window.addEventListener('load', function() {
             context.fillStyle = this.color;
             context.shadowOffsetX = 2;
             context.shadowOffsetY = 2;
-            context.shadowColor = 'black';
+            context.shadowColor = 'black'; 
             context.font = this.fontSize + 'px' + this.fontFamily;
             //score
             context.fillText('Score' + this.game.score, 20, 40);
             //ammo
             for (let i = 0; i < this.game.ammo; i++){
                 context.fillRect(20 + 5*i, 50, 3, 20);
+            }
+            //timer
+            // Using toFixed Method to clear the additional number of digits on the screen
+            const formattedTime = (this.game.gameTime * 0.001).toFixed(1);
+            context.fillText('Timer: ' + formattedTime, 20, 100);
+            // game over messages
+            if (this.game.gameOver){
+                context.textAlign = 'center';
+                let message1;
+                let message2;
+                if (this.game.score > this.game.winningScore){
+                    message1 = 'You Won!';
+                    message2 = 'Well Done!';
+                } else {
+                    message1 = 'You lost!';
+                    message2 = 'Try again next time!';
+                }
+                context.font = '150px ' + this.fontFamily;
+                context.fillText(message1, this.game.width * 0.5, this.game.height * 0.5 - 40);
+                context.font = '75px ' + this.fontFamily;
+                context.fillText(message2, this.game.width * 0.5, this.game.height * 0.5 + 80);
             }
             context.restore();
         }
@@ -161,8 +182,12 @@ window.addEventListener('load', function() {
             this.gameOver = false;
             this.score = 0;
             this.winningScore = 10;
+            this.gameTime = 0;
+            this.timeLimit = 50000;
         }
         update(deltaTime){
+            if (!this.gameOver) this.gameTime += deltaTime;
+            if (this.gameTime > this.timeLimit) this.gameOver = true;
             this.player.update();
             if (this.ammoTimer > this.ammoInterval){
                 if (this.ammo < this.maxAmmo) this.ammo++;
@@ -181,7 +206,7 @@ window.addEventListener('load', function() {
                         projectile.markedForDeletion = true;
                         if (enemy.lives <= 0){
                             enemy.markedForDeletion = true;
-                            this.score += enemy.score;
+                            if (!this.gameOver) this.score += enemy.score;
                             if (this.score > this.winningScore) this.gameOver = true;
                         }
                     }
