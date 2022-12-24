29b80d72aee2c61bc7dbaff3d4851d33dad67c1f
Parsa Peikani <a12365@Parsas-MacBook-Air.local>
2022-12-24 15:34:46 -0800

    Making some tweaks and fixes

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ window.addEventListener('load', function() {
             this.angle = 0;
             this.va = Math.random() * 0.2 - 0.1;
             this.bounced = 0;
-            this.bottomBounceBoundary = Math.random() * 100 + 60;
+            this.bottomBounceBoundary = Math.random() * 80 + 60;
         }
         update(){
             this.angle += this.va;
@@ -162,7 +162,7 @@ window.addEventListener('load', function() {
         enterPowerUp(){
             this.powerUpTimer = 0;
             this.powerUp =true;
-            this.game.ammo = this.game.maxAmmo;
+            if (this.game.ammo < this.game.maxAmmo) this.game.ammo = this.game.maxAmmo;
         }
     }
     class Enemy {
@@ -345,7 +345,7 @@ window.addEventListener('load', function() {
             this.gameTime = 0;
             this.timeLimit = 50000;
             this.speed = 1;
-            this.debug = true;
+            this.debug = false;
         }
         update(deltaTime){
             if (!this.gameOver) this.gameTime += deltaTime;
@@ -369,7 +369,7 @@ window.addEventListener('load', function() {
                         this.particles.push(new Particle(this, enemy.x + 
                         enemy.width * 0.5, enemy.y + enemy.height * 0.5));
                     }
-                    if (enemy.type = 'lucky') this.player.enterPowerUp();
+                    if (enemy.type === 'lucky') this.player.enterPowerUp();
                     else this.score--;
                 }
                 this.player.projectiles.forEach(projectile => {
@@ -400,9 +400,9 @@ window.addEventListener('load', function() {
         }
         draw(context){
             this.background.draw(context);
+            this.UI.draw(context);
             context.fillStyle = 'black';
             this.player.draw(context);
-            this.UI.draw(context);
             this.particles.forEach(particle => particle.draw(context));
             this.enemies.forEach(enemy => {
                 enemy.draw(context);
